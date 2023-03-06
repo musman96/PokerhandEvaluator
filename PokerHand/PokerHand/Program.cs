0152eb0bd0580dc7f3a0008f114f6d94e49d7046
Musa Sibuyi <sibuyimusa@gmail.com>
2023-03-06 05:12:18 +0200

    clean up code and add comments

--- PokerHand/PokerHand/Program.cs
+++ PokerHand/PokerHand/Program.cs
@@ -8,6 +8,17 @@ namespace PokerHand
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Poker Hand Evaluator!");
+            Console.WriteLine(@"The tool uses the following symbols to represent a suit for the cards:
+'C' or 'c' for Clubs
+'D' or 'd' for Diamonds
+'H' or 'h' for Hearts
+'S' or 's' for Spades");
+            Console.WriteLine(@"These are the symbols for the following cards:
+'T' for Ten,
+'J' for Jack,
+'Q' for Queen,
+'K' for King,
+'A' for Ace");
             Console.WriteLine("Please enter your hand in the format of '2H 3D 5S 9C KD':");
 
             string input = Console.ReadLine();
@@ -18,27 +29,40 @@ namespace PokerHand
             var result = evaluator.EvaluateHand(cards);
             var handRepresentation = evaluator.EvaluatedHandRepresentation(result);
             Console.WriteLine("Your hand is a {0}", result);
-            Console.WriteLine("String representation of the hand is {0}", handRepresentation);
+            Console.WriteLine("String representation of the hand is a {0}", handRepresentation);
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// This function takes the string input provided, splits the string into an array of the cards
+        /// It then loops through each card to determined the rank and suit
+        /// Uses enum to match the two characters to determine the rank and suit
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>a list of cards in a format like this: Two Hearts</returns>
+        /// <exception cref="ArgumentException"></exception>
         static List<Card> ParseInput(string input)
         {
             var cards = new List<Card>();
             var inputs = input.Split(' ');
 
+            //loop through the array of inputs and determine the exact card using the rank and suit
+            //add the card to a list
             foreach (var card in inputs)
             {
                 var rankString = card.Substring(0, card.Length - 1);
                 var suitString = card.Substring(card.Length - 1);
-
+                
+                //check if the card is valid. a valid card will have 2 characters(rank and suit)
                 if (card.Length != 2)
                 {
                     throw new ArgumentException("Invalid input");
                 }
 
-                var rankChar = card[0];
-                var suitChar = card[1];
+                var rankChar = card[0];//first character of the card
+                var suitChar = card[1];//second character of the card
+
+                // Determine the rank of the card based on its first character.
                 Rank rank;
                 switch (rankChar)
                 {
@@ -85,6 +109,7 @@ namespace PokerHand
                         throw new ArgumentException("Invalid card rank.");
                 }
 
+                // Determine the suit of the card based on its second character.
                 SuiteEnum suit;
                 switch (suitChar)
                 {
@@ -111,6 +136,8 @@ namespace PokerHand
                 cards.Add(new Card(rank, suit));
             }
 
+            /*If the list of cards does not contain exactly 5 elements, throw an ArgumentException.
+             This can of course be changed to evaluate the number of cards. for now as default, we are evaluating against 5 cards*/
             if (cards.Count != 5)
             {
                 throw new ArgumentException("Invalid number of cards: " + cards.Count);
--- PokerHand/PokerHand/Services/PokerHandEvaluator.cs
+++ PokerHand/PokerHand/Services/PokerHandEvaluator.cs
@@ -22,6 +22,11 @@ namespace PokerHand.Services
             new OnePairHandEvaluator()
         };
 
+        /// <summary>
+        /// Evaluates the poker hand from the list of cards provided
+        /// </summary>
+        /// <param name="cards"></param>
+        /// <returns>an integer value signifying the hand evaluated from the list of cards</returns>
         public int EvaluateHand(List<Card> cards)
         {
             var validHandEvaluators = _handEvaluators.Where(he => he.IsValidHand(cards)).ToList();
@@ -45,101 +50,39 @@ namespace PokerHand.Services
             switch (hand)
             {
                 case 1:
-                    name ="High Card";
+                    name ="One Pair";
                     break;
                 case 2:
-                    name = "One Pair";
-                    break;
-                case 3:
                     name = "Two Pair";
                     break;
-                case 4:
-                    name = "Three of a Kind";
+                case 3:
+                    name = "Three of a kind";
                     break;
-                case 5:
+                case 4:
                     name = "Straight";
                     break;
-                case 6:
+                case 5:
                     name = "Flush";
                     break;
-                case 7:
+                case 6:
                     name = "Full House";
                     break;
-                case 8:
+                case 7:
                     name = "Four of a kind";
                     break;
-                case 9:
+                case 8:
                     name = "Straight Flush";
                     break;
-                case 10:
+                case 9:
                     name = "Royal Flush";
                     break;
                 default:
-                    name = "none";
+                    name = "High Card";
                     break;
             }
 
             return name;
         }
-
-        //public static (int, string) EvaluateHand(List<Card> cards)
-        //{
-        //    if (cards.Count != 5)
-        //    {
-        //        throw new ArgumentException("Invalid number of cards");
-        //    }
-        //
-        //    var flushEvaluator = new FlushHandEvaluator();
-        //    var straightEvaluator = new StraightHandEvaluator();
-        //    var pairEvaluator = new PairHandEvaluator();
-        //
-        //    if (flushEvaluator.HasHand(cards) && straightEvaluator.HasHand(cards) && cards.Any(c => c.Rank == Rank.Ace))
-        //    {
-        //        return (10, "Royal Flush");
-        //    }
-        //
-        //    if (flushEvaluator.HasHand(cards) && straightEvaluator.HasHand(cards))
-        //    {
-        //        return (9, "Straight Flush");
-        //    }
-        //
-        //    if (pairEvaluator.HasHand(cards, 4))
-        //    {
-        //        return (8, "Four of a Kind");
-        //    }
-        //
-        //    if (pairEvaluator.HasHand(cards, 3) && pairEvaluator.HasHand(cards, 2))
-        //    {
-        //        return (7, "Full House");
-        //    }
-        //
-        //    if (flushEvaluator.HasHand(cards))
-        //    {
-        //        return (6, "Flush");
-        //    }
-        //
-        //    if (straightEvaluator.HasHand(cards))
-        //    {
-        //        return (5, "Straight");
-        //    }
-        //
-        //    if (pairEvaluator.HasHand(cards, 3))
-        //    {
-        //        return (4, "Three of a Kind");
-        //    }
-        //
-        //    if (pairEvaluator.HasHand(cards, 2) && pairEvaluator.CountPairs(cards) == 2)
-        //    {
-        //        return (3, "Two Pair");
-        //    }
-        //
-        //    if (pairEvaluator.HasHand(cards, 2))
-        //    {
-        //        return (2, "Pair");
-        //    }
-        //
-        //    return (1, "High Card");
-        //}
 
     }
 }
