863f57502708a8d6ff7bc9e9a0bb703216310bb4
Musa Sibuyi <sibuyimusa@gmail.com>
2023-03-06 03:05:20 +0200

    rewrite the program class for user input

--- PokerHand/PokerHand/Models/Card.cs
+++ PokerHand/PokerHand/Models/Card.cs
@@ -10,5 +10,10 @@ namespace PokerHand.Models
     {
         public Rank Rank { get; set; }
         public Suit Suit { get; set; }
+        public Card(Rank rank, Suit suit)
+        {
+            Rank = rank;
+            Suit = suit;    
+        }
     }
 }
--- PokerHand/PokerHand/Program.cs
+++ PokerHand/PokerHand/Program.cs
@@ -7,38 +7,62 @@ namespace PokerHand
     {
         static void Main(string[] args)
         {
-            // Create a deck of cards
-            var deck = new List<Card>();
-            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+            Console.WriteLine("Welcome to Poker Hand Evaluator!");
+            Console.WriteLine("Please enter your hand in the format of '2H 3D 5S 9C KD':");
+
+            string input = Console.ReadLine();
+            var cards = ParseInput(input);
+
+            var evaluator = new PokerHandEvaluator();
+
+            var result = evaluator.EvaluateHand(cards);
+
+            Console.WriteLine("Your hand is a {0}", result);
+            Console.ReadLine();
+        }
+
+        static List<Card> ParseInput(string input)
+        {
+            var cards = new List<Card>();
+            var inputs = input.Split(' ');
+
+            foreach (var card in inputs)
             {
-                foreach (var suitName in new[] { "Hearts", "Diamonds", "Clubs", "Spades" })
+                var rankString = card.Substring(0, card.Length - 1);
+                var suitString = card.Substring(card.Length - 1);
+
+                if (card.Length != 2)
                 {
-                    var card = new Card
-                    {
-                        Rank = rank,
-                        Suit = new Suit(suitName)
-                    };
-                    deck.Add(card);
+                    throw new ArgumentException("Invalid input");
                 }
+
+                var rankChar = input[0];
+                var suitChar = input[1];
+                Rank rank;
+                if (!Enum.TryParse<Rank>(rankChar.ToString(), out rank))
+                {
+                    throw new ArgumentException("Invalid card rank: " + rankString);
+                }
+
+                Suit suit;
+                try
+                {
+                    suit = (Suit)Enum.Parse(typeof(Suit), suitChar.ToString(), true);
+                }
+                catch (ArgumentException)
+                {
+                    throw new ArgumentException("Invalid card suit: " + suitString);
+                }
+
+                cards.Add(new Card(rank, suit));
             }
 
-            // Shuffle the deck
-            var random = new Random();
-            deck = deck.OrderBy(c => random.Next()).ToList();
+            if (cards.Count != 5)
+            {
+                throw new ArgumentException("Invalid number of cards: " + cards.Count);
+            }
 
-            // Draw a hand of five cards
-            var hand = deck.Take(5).ToList();
-
-            // Evaluate the hand
-            var handEvaluator = new PokerHandEvaluator();
-            var handRank = handEvaluator.EvaluateHand(hand);
-
-            // Print the hand
-
-            Console.WriteLine($"Evaluated Poker Hand: {0}",handRank);
-
-            //keep the Console open until closed by user
-            Console.ReadLine();
+            return cards;
         }
     }
 }
