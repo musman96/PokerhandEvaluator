58df06008c7820d51ffed8d8b421d4ac473d12e8
Musa Sibuyi <sibuyimusa@gmail.com>
2023-03-06 05:39:01 +0200

    Add intial tests for Full House and Royal Flush evaluations

---
+++ PokerHand/PokerHandTests/EvaluateRoyalFlushTest.cs
@@ -0,0 +1,62 @@
+﻿using PokerHand.Models;
+using PokerHand.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandTests
+{
+    [TestClass]
+    public class EvaluateRoyalFlushTest
+    {
+        [TestMethod]
+        public void TestEvaluateRoyalFlush()
+        {
+            // Arrange
+            var evaluator = new PokerHandEvaluator();
+            var cards = new List<Card>
+            {
+                new Card(Rank.Ace, SuiteEnum.Spades),
+                new Card(Rank.King, SuiteEnum.Spades),
+                new Card(Rank.Queen, SuiteEnum.Spades),
+                new Card(Rank.Jack, SuiteEnum.Spades),
+                new Card(Rank.Ten, SuiteEnum.Spades)
+            };
+
+            // Act
+            var result = evaluator.EvaluateHand(cards);
+            var handRepresentation = evaluator.EvaluatedHandRepresentation(result);
+            // Assert
+            Assert.AreEqual(9, result);
+            Assert.AreEqual("Royal Flush", handRepresentation);
+        }
+
+        [TestMethod]
+        public void TestEvaluateNonRoyalFlush()
+        {
+            // Arrange
+            var evaluator = new PokerHandEvaluator();
+            var cards = new List<Card>
+            {
+                new Card(Rank.Ace, SuiteEnum.Spades),
+                new Card(Rank.King, SuiteEnum.Spades),
+                new Card(Rank.Queen, SuiteEnum.Spades),
+                new Card(Rank.Jack, SuiteEnum.Spades),
+                new Card(Rank.Nine, SuiteEnum.Diamonds) // Not a ten, so not a royal flush
+            };
+
+            // Act
+            var result = evaluator.EvaluateHand(cards);
+            var handRepresentation = evaluator.EvaluatedHandRepresentation(result);
+
+            // Assert
+            Assert.AreNotEqual(9, result);
+            Assert.AreNotEqual("Royal Flush", handRepresentation);
+        }
+
+
+    }
+
+}
---
+++ PokerHand/PokerHandTests/FullHouseTests.cs
@@ -0,0 +1,58 @@
+﻿using PokerHand.Models;
+using PokerHand.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerHandTests
+{
+    [TestClass]
+    public class FullHouseTests
+    {
+        [TestMethod]
+        public void FullHouseHandEvaluator_PositiveTest()
+        {
+            // Arrange
+            var cards = new List<Card>
+    {
+        new Card(Rank.Ten, SuiteEnum.Diamonds),
+        new Card(Rank.Ten, SuiteEnum.Clubs),
+        new Card(Rank.Ten, SuiteEnum.Hearts),
+        new Card(Rank.Seven, SuiteEnum.Diamonds),
+        new Card(Rank.Seven, SuiteEnum.Spades)
+    };
+            var evaluator = new PokerHandEvaluator();
+
+            // Act
+            var result = evaluator.EvaluateHand(cards);
+            // Assert
+            Assert.AreEqual(6,result);
+        }
+
+        [TestMethod]
+        public void FullHouseHandEvaluator_NegativeTest()
+        {
+            // Arrange
+            var cards = new List<Card>
+    {
+        new Card(Rank.Ten, SuiteEnum.Diamonds),
+        new Card(Rank.Ten, SuiteEnum.Clubs),
+        new Card(Rank.Nine, SuiteEnum.Hearts),
+        new Card(Rank.Seven, SuiteEnum.Diamonds),
+        new Card(Rank.Seven, SuiteEnum.Spades)
+    };
+            var evaluator = new PokerHandEvaluator();
+
+            // Act
+            var result = evaluator.EvaluateHand(cards);
+            var handRepresentation = evaluator.EvaluatedHandRepresentation(result);
+
+            // Assert
+            Assert.AreNotEqual(5, result);
+        }
+
+
+    }
+}
